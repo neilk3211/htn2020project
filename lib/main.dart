5adc4b55862b4c15c835c0d16ab4b706dc5caf55
Neil Kaus <neilk1729@gmail.com>
2021-01-16 15:18:09 -0500

    initial skelton

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:htn2020project/pages/add_task.dart';
+import 'package:htn2020project/pages/task_list.dart';
 
-void main() => runApp(MaterialApp());
+void main() => runApp(MaterialApp(
+  initialRoute: '/',
+  routes: {
+    '/': (context) => TaskList(),
+    '/addtask': (context) => AddTask()
+  },
+));
---
+++ lib/pages/task_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskList extends StatefulWidget {
+  @override
+  _TaskListState createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(  child: Text('task list'))
+    );
+  }
+}
