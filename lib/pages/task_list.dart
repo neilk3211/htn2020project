382e0b55d9621b79c326a46b1dcf66b4d04c10d8
James Chen <jchen9827@gmail.com>
2021-01-16 16:58:52 -0500

    Task List Basic Appearance
    - Created basic layout of task list
    - List of tasks still need to be user-defined
    - List of tasks need to lead to edit page

--- lib/pages/task_list.dart
+++ lib/pages/task_list.dart
@@ -6,10 +6,58 @@ class TaskList extends StatefulWidget {
 }
 
 class _TaskListState extends State<TaskList> {
+  List<Tasks> tasknames = [
+    Tasks(url: 'Europe/London', taskname: 'English project'),
+    Tasks(url: 'Europe/Berlin', taskname: 'Math project'),
+    Tasks(url: 'Africa/Cairo', taskname: 'Science project'),
+    Tasks(url: 'Africa/Nairobi', taskname: 'Geography project'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(  child: Text('task list'))
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Your Tasks'),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                Navigator.pushNamed(context, '/addtask');
+              });
+            },
+            child: Icon(Icons.add),
+            backgroundColor: Colors.grey[800]
+        ),
+        body: ListView.builder(
+            itemCount: tasknames.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal:4.0),
+                child: Card(
+                  child:ListTile(
+                    onTap: () {
+                      print(tasknames[index].taskname);
+                      Navigator.pushNamed(context, '/addtask');//need to change to edit
+                    },
+                    title: Text(tasknames[index].taskname),
+                  ),
+                ),
+              );
+            }
+        )
     );
   }
+}
+
+class Tasks {
+
+  String taskname; // task name for UI
+  String url; // task url for api endpoint
+
+  Tasks({ this.taskname, this.url });
+
 }
