41d89af5443de4c530154ae245231fb714b3a6c7
Neil Kaus <neilk1729@gmail.com>
2021-01-16 16:28:33 -0500

    prototype calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:htn2020project/pages/add_task.dart';
+import 'package:htn2020project/pages/calendar.dart';
 import 'package:htn2020project/pages/edit_task.dart';
 import 'package:htn2020project/pages/task_list.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/edittask',
+  initialRoute: '/',
   routes: {
     '/': (context) => TaskList(),
     '/addtask': (context) => AddTask(),
     '/edittask': (context) => EditTask(),
+    '/calendar': (context) => Calendar(),
   },
 ));
