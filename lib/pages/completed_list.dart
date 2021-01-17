3de7d7a670062efe256f97be5f17a8b5c545da6f
James Chen <jchen9827@gmail.com>
2021-01-17 10:59:53 -0500

    test

--- lib/pages/completed_list.dart
+++ lib/pages/completed_list.dart
@@ -6,6 +6,14 @@ class CompletedList extends StatefulWidget {
 }
 
 class _CompletedListState extends State<CompletedList> {
+  List<Tasks> tasknames = [
+    Tasks(url: 'Europe/London', taskname: 'English project', remainingtime: 3600),
+    Tasks(url: 'Europe/Berlin', taskname: 'Math project', remainingtime: 3600),
+    Tasks(url: 'Africa/Cairo', taskname: 'Science project', remainingtime: 7200),
+    Tasks(url: 'Africa/Nairobi', taskname: 'Geography project', remainingtime: 2400),
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,4 +23,15 @@ class _CompletedListState extends State<CompletedList> {
       ),
     );
   }
+}
+
+class Tasks {
+
+  String taskname; // task name for UI
+  String url; // task url for api endpoint - no function at the moment
+  double remainingtime; //in seconds
+  bool complete = false;
+
+  Tasks({ this.taskname, this.url, this.remainingtime });
+
 }
