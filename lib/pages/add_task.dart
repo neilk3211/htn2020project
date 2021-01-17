0bb6a48e8bfd58efb63a97be278d4acccf952493
calmelanson <77516391+calmelanson@users.noreply.github.com>
2021-01-16 19:13:37 -0500

    updated add task page
    unfinished, still working on it

--- lib/pages/add_task.dart
+++ lib/pages/add_task.dart
@@ -1,15 +1,106 @@
 import 'package:flutter/material.dart';
 
-class AddTask extends StatefulWidget {
+void main() => runApp(MaterialApp(
+  home: CreateNewTask(),
+));
+
+/*testing stuff here
+int maxLength = 2;
+final bool maxLengthEnforced = true;*/
+
+class CreateNewTask extends StatefulWidget {
   @override
-  _AddTaskState createState() => _AddTaskState();
+  _CreateNewTaskState createState() => _CreateNewTaskState();
 }
 
-class _AddTaskState extends State<AddTask> {
+class _CreateNewTaskState extends State<CreateNewTask> {
+/* testing controller stuff here, don't worry about this for now
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('add task'))
+      appBar: AppBar(
+        title: Text('Create New Task'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              child: Text(
+                'Name of task',
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            Container(
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20.0),
+              child: Text(
+                'Amount of time needed',
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Flexible(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Flexible(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Flexible(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
